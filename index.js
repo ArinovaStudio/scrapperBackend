8d48b68e8898412551392d98df48a3ac08b841bd
Danish-Devx3 <danish.devx3@gmail.com>
2025-10-30 17:05:35 +0530

    Revert "Merge branch 'main' into scrap-d"
    This reverts commit 86adda7f3b4c8ce37681229d9ccf4729d1f7e717, reversing
    changes made to 3dc08747b5b332e2a4738a57bb8935071a3cd758.

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import express, { response } from "express";
+import express from "express";
 import axios from "axios";
 import bodyParser from "body-parser";
 import dotenv from "dotenv";
@@ -10,9 +10,6 @@ app.use(bodyParser.json());
 
 // Google Maps API base URL
 const GOOGLE_PLACES_API = "https://maps.googleapis.com/maps/api/place/nearbysearch/json";
-
-// Perplexity API base URL
-const PERPLEXITY_API = "https://api.perplexity.ai/chat/completions";
 
 /**
  * POST /scrap
@@ -76,97 +73,6 @@ app.post("/scrap", async (req, res) => {
     res.status(500).json({ error: "Failed to fetch nearby places" });
   }
 });
-
-/**
- * POST /research
- * Body: {
- *   "name": "Peter Luger Steak House",
- *   "location": "255 Northern Boulevard, Great Neck",
- *   "rating": 4.5,
- *   "userRatingsTotal": 3725,
- *   "location": null,
- *   "placeId": "ChIJKXFLL-uJwokREGE5L-3oRxc"
- * }
- */
-app.post("/research", async (req, res) => {
-  try {
-    const reqData = req.body;
-
-    // Checking the body length.
-    if (!reqData.results.length) {
-      return res.status(401).json({ error: "No Data found to do research" });
-    }
-
-    // Converting it to JSON.
-    const placesJSON = JSON.stringify(reqData.results, null, 2);
-
-    // Making request to perplexity.
-    const researchResp = await axios.post(PERPLEXITY_API,
-      {
-        model: "sonar",
-        messages: [
-          {
-            role: "user",
-            content: `
-              Analyze the following list of places. For each, give:
-              - A short summary (1-2 Lines)
-              - Key highlights or special features
-              - Whether it's popular among locals or tourists
-
-              Return the result **as JSON** in this format:
-              [
-                {
-                  "name": "...",
-                  "summary": "...",
-                  "highlights": ["..."],
-                  "popularity": "..."
-                }
-              ]
-
-              Here's the Data:
-              ${placesJSON}
-            `
-          }
-        ]
-      },
-      {
-        headers: {
-          "Content-Type": "application/json",
-          "Authorization": `Bearer ${process.env.PERPLEXITY_API_KEY}`
-        }
-      });
-
-    const result = researchResp.data.choices[0].message.content;
-    return res.status(200).json({ response: extractJSON(result) });
-
-  } catch (error) {
-    console.error("Error:", error.response?.data || error.message);
-    res.status(500).json({ error: "Failed to do research" });
-  }
-})
-
-/**
- * extractJSON: Extracts the Invalid JSON.
- * @param {*} text
- * @returns
- */
-const extractJSON = (text) => {
-  try {
-    // Matching the JSON.
-    const jsonMatch = text.match(/```json([\s\S]*?)```/);
-    const cleanText = jsonMatch ? jsonMatch[1] : text;
-
-    // Cleaning up the JSON if any invalid character founds.
-    const validJSON = cleanText
-      .trim()
-      .replace(/,\s*([\]}])/g, "$1");
-
-    return JSON.parse(validJSON)
-  } catch (err) {
-    console.error("Failed to parse JSON:", err.message);
-    return { error: "Invalid JSON returned from model", raw: text };
-  }
-}
 
 const PORT = process.env.PORT || 8000;
 app.listen(PORT, () => console.log(`✅ Server running on http://localhost:${PORT}`));
