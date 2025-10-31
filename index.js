68cd885b03b94ed62251ab244f119c1488858493
Danish-Devx3 <danish.devx3@gmail.com>
2025-10-31 14:46:42 +0530

    feat(research): implement endpoint for analyzing places with Perplexity API

--- index.js
+++ index.js
@@ -97,5 +97,82 @@ app.post("/scrap", async (req, res) => {
   }
 });
 
+app.post("/research", async (req, res) => {
+  const PERPLEXITY_API = "https://api.perplexity.ai/chat/completions";
+  try {
+    const reqData = req.body;
+    
+    // Checking the body length.
+    if (!reqData.results.length) {
+      return res.status(401).json({ error: "No Data found to do research" });
+    }
+    
+    // Converting it to JSON.
+    const placesJSON = JSON.stringify(reqData.results, null, 2);
+    
+    // Making request to perplexity.
+    const researchResp = await axios.post(PERPLEXITY_API,
+      {
+        model: "sonar",
+        messages: [
+          {
+            role: "user",
+            content: `
+            Analyze the following list of places. For each, give:
+            - A short summary (1-2 Lines)
+            - Key highlights or special features
+            - Whether it's popular among locals or tourists
+            
+            Return the result **as JSON** in this format:
+            [
+              {
+                "name": "...",
+                "summary": "...",
+                "highlights": ["..."],
+                "popularity": "..."
+                }
+                ]
+                
+                Here's the Data:
+                ${placesJSON}
+                `
+              }
+            ]
+          },
+          {
+            headers: {
+              "Content-Type": "application/json",
+              "Authorization": `Bearer ${process.env.PERPLEXITY_API}`
+            }
+          });
+          
+          const result = researchResp.data.choices[0].message.content;
+          console.log(result)
+          return res.status(200).json({ response: extractJSON(result) || result });
+          
+        } catch (error) {
+          console.error("Error:", error.response?.data || error.message);
+    res.status(500).json({ error: "Failed to do research" });
+  }
+})
+
+const extractJSON = (text) => {
+  try {
+    // Matching the JSON.
+    const jsonMatch = text.match(/```json([\s\S]*?)```/);
+    const cleanText = jsonMatch ? jsonMatch[1] : text;
+
+    // Cleaning up the JSON if any invalid character founds.
+    const validJSON = cleanText
+      .trim()
+      .replace(/,\s*([\]}])/g, "$1");
+
+    return JSON.parse(validJSON)
+  } catch (err) {
+    console.error("Failed to parse JSON:", err.message);
+    return { error: "Invalid JSON returned from model", raw: text };
+  }
+}
+
 const PORT = process.env.PORT || 8000;
 app.listen(PORT, () => console.log(`✅ Server running on http://localhost:${PORT}`));
